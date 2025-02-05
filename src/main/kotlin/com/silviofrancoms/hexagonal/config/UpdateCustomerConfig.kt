8a476199c1810e7e5c7840a51222a517db75f600
silviofranco <silviofrancoms@gmail.com>
2025-02-05 14:47:06 -0400

    Criação CRUD - Update - Criando o bean

---
+++ src/main/kotlin/com/silviofrancoms/hexagonal/config/UpdateCustomerConfig.kt
@@ -0,0 +1,24 @@
+package com.silviofrancoms.hexagonal.config
+
+import com.silviofrancoms.hexagonal.adapters.out.FindAddressByZipCodeAdapter
+import com.silviofrancoms.hexagonal.adapters.out.UpdateCustomerAdapter
+import com.silviofrancoms.hexagonal.application.core.usecase.FindCustomerByIdUseCase
+import com.silviofrancoms.hexagonal.application.core.usecase.UpdateCustomerUseCase
+import com.silviofrancoms.hexagonal.application.ports.`in`.UpdateCustomerInputPort
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class UpdateCustomerConfig {
+
+    @Bean
+    fun updateCustomer(
+        findCustomerByIdUseCase: FindCustomerByIdUseCase,
+        findAddressByZipCodeAdapter: FindAddressByZipCodeAdapter,
+        updateCustomerAdapter: UpdateCustomerAdapter
+    ): UpdateCustomerInputPort = UpdateCustomerUseCase(
+        findCustomerByIdUseCase,
+        findAddressByZipCodeAdapter,
+        updateCustomerAdapter
+    )
+}
