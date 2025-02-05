386ad6181e6924699bb63d2bdba967ca49958a5b
silviofranco <silviofrancoms@gmail.com>
2025-02-05 18:53:30 -0400

    Kafka - Enviando o CPF para validação na alteração

--- src/main/kotlin/com/silviofrancoms/hexagonal/application/core/usecase/UpdateCustomerUseCase.kt
+++ src/main/kotlin/com/silviofrancoms/hexagonal/application/core/usecase/UpdateCustomerUseCase.kt
@@ -4,22 +4,27 @@ import com.silviofrancoms.hexagonal.application.core.domain.Customer
 import com.silviofrancoms.hexagonal.application.ports.`in`.FindCustomerByIdInputPort
 import com.silviofrancoms.hexagonal.application.ports.`in`.UpdateCustomerInputPort
 import com.silviofrancoms.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort
+import com.silviofrancoms.hexagonal.application.ports.out.SendCpfForValidationOutputPort
 import com.silviofrancoms.hexagonal.application.ports.out.UpdateCustomerOutputPort
 
 class UpdateCustomerUseCase(
     private val findCustomerByIdInputPort: FindCustomerByIdInputPort,
     private val findAddressByZipCodeOutputPort: FindAddressByZipCodeOutputPort,
-    private val updateCustomerOutputPort: UpdateCustomerOutputPort
+    private val updateCustomerOutputPort: UpdateCustomerOutputPort,
+    private val sendCpfForValidationOutputPort: SendCpfForValidationOutputPort
 ) : UpdateCustomerInputPort {
 
     override fun update(customer: Customer, zipCode: String) {
         if (customer.id == null) throw IllegalArgumentException("Id field cannot be null")
-        findCustomerByIdInputPort.find(customer.id)
+        val savedCpf = findCustomerByIdInputPort.find(customer.id).cpf
 
         customer.apply {
             address = findAddressByZipCodeOutputPort.find(zipCode)
         }.let {
             updateCustomerOutputPort.update(it)
+            if (savedCpf != it.cpf){
+                sendCpfForValidationOutputPort.send(it.cpf)
+            }
         }
     }
 }
